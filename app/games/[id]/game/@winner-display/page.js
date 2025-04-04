d067673832c22e6bac892a2233aae16340dc36d2
Aswatth <aswatth56@gmail.com>
2025-04-04 19:27:58 -0400

    Update page.js
    - Added reply functionality.

--- app/games/[id]/game/@winner-display/page.js
+++ app/games/[id]/game/@winner-display/page.js
@@ -3,6 +3,10 @@ import { useRouter } from "next/navigation";
 import styles from "./page.module.css";
 
 import PropTypes from "prop-types";
+import { clientStore } from "@/app/(utils)/data-stores/webSocketStore";
+import { useEffect } from "react";
+import { gameStore, lobbyStore } from "@/app/(utils)/data-stores/gameStore";
+import { playerStore } from "@/app/(utils)/data-stores/playerStore";
 
 WinnerDialog.propTypes = {
   message: PropTypes.string,
@@ -11,6 +15,32 @@ WinnerDialog.propTypes = {
 
 export default function WinnerDialog({ message, showReplayButton }) {
   const router = useRouter();
+  const { lobby, setLobby } = lobbyStore();
+  const { setReadyStatus } = playerStore();
+  const { client } = clientStore();
+
+  useEffect(() => {
+    const subscription = client.subscribe(
+      "/topic/lobby/" + lobby.gameId,
+      (response) => {
+        const content = JSON.parse(response.body);
+        setLobby(content);
+        setReadyStatus(content.status);
+
+        router.back();
+      }
+    );
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, []);
+
+  function handleReplay() {
+    client.publish({
+      destination: "/app/game/" + lobby.gameId + "/replay",
+    });
+  }
 
   return (
     <div className={styles.background}>
@@ -20,7 +50,7 @@ export default function WinnerDialog({ message, showReplayButton }) {
           <button
             className={styles.replayButton}
             onClick={() => {
-              router.back();
+              handleReplay();
             }}
           >
             REPLAY
